cf6013c38ee190530ec73d5c52f856f6472a4901
Reine0017 <m.fortitudo.fr@gmail.com>
2020-08-27 21:52:48 +0800

    removed switch

--- react_frontend/src/App.js
+++ react_frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { Switch, Route } from "react-router-dom";
+import { Route } from "react-router-dom";
 
 import HomePage from "./pages/HomePage/HomePage";
 import AboutPage from "./pages/AboutPage/AboutPage";
@@ -9,10 +9,8 @@ function App() {
   return (
     <div className="App">
       <span>HELLO WORLD</span>
-      <Switch>
-        <Route exact path="/AboutPage" component={AboutPage} />
-        <Route exact path="/" component={HomePage} />
-      </Switch>
+      <Route exact path="/AboutPage" component={AboutPage} />
+      <Route exact path="/" component={HomePage} />
     </div>
   );
 }
